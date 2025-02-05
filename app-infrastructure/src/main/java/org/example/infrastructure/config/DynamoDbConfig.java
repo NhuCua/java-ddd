e473227b227616e92f87408033e74f58329d357e
Tom <cuale@fusiontech.company>
2025-02-05 14:09:12 +0700

    add env dynamo db config

--- app-infrastructure/src/main/java/org/example/infrastructure/config/DynamoDbConfig.java
+++ app-infrastructure/src/main/java/org/example/infrastructure/config/DynamoDbConfig.java
@@ -1,5 +1,6 @@
 package org.example.infrastructure.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
@@ -7,16 +8,30 @@ import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
+import java.net.URI;
+
 @Configuration
 public class DynamoDbConfig {
+    @Value("${aws.accessKeyId}")
+    private String accessKeyId;
+
+    @Value("${aws.secretAccessKey}")
+    private String secretAccessKey;
+
+    @Value("${aws.region}")
+    private String region;
+
+    @Value("${aws.dynamodb.endpoint}")
+    private String dynamoDbEndpoint;
+
     @Bean
     public DynamoDbClient dynamoDbClient() {
         return DynamoDbClient.builder()
-                .region(Region.US_EAST_1)
+                .region(Region.of(region))
                 .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create("fakeMyKeyId", "fakeSecretAccessKey")
+                        AwsBasicCredentials.create(accessKeyId, secretAccessKey)
                 ))
-                .endpointOverride(java.net.URI.create("http://localhost:8000"))
+                .endpointOverride(URI.create(dynamoDbEndpoint))
                 .build();
     }
 }
